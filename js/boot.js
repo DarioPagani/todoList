6ecff17058464c8a88a89c0d47b6370352a0080b
Dario Pagani <dario.pagani.146@gmail.com>
2017-06-09 14:39:11 +0200

    Aggiunto un'altro file

---
+++ js/boot.js
@@ -0,0 +1,12 @@
+/** Migliarino Pisano, li 09/06/2017
+ * Questo file contiene le istruzioni
+ * @author: Dario Pagani <dario.pagani@itispisa.gov.it>, Nordine Oubihi <nordine.oubihi@itispisa.gov.it>
+ * @version: 0.01a
+ */
+
+// TODO Scrivere questo file
+if(localStorage === undefined)
+{
+	alert("Questa piattaforma non supporta il LocalStorage");
+	return -1;
+}
