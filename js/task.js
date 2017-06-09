0834b6866210a7b6ed59854bfa7ea214a2990481
Dario Pagani <dario.pagani.146@gmail.com>
2017-06-09 14:31:38 +0200

    Aggiunti file

---
+++ js/task.js
@@ -0,0 +1,24 @@
+/** Migliarino Pisano, li 09/06/2017 
+ * Questo file contiene la classe "Task" per gestire la gestione di un singolo 
+ *  task
+ *  @author: Dario Pagani <dario.pagani@itispisa.gov.it>, Nordine Oubihi <nordine.oubihi@itispisa.gov.it>
+ *  @version: 0.00a
+ */
+
+function Task()
+{
+	/****** COSTRUTTORE *******/
+	// TODO Scrivere il costruttore :P
+
+
+
+
+	/****** METODI **********/
+	// TODO Scrivere i metodi
+	this.setScadenza = function() {;};
+	
+	this.setTitolo = function() {;};
+	
+	this.toString = function() {;};
+	this.toStringHTML = function() {return "<p>Nope</p>"};
+}
