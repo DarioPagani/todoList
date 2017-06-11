9b9cefb29f076ae17724950a8c2cd776d723a23c
DarioPagani <dariopag45@gmail.com>
2017-06-11 19:07:21 +0200

    Aggiunta un po' di roba perché mi annoio

---
+++ js/addTask.js
@@ -0,0 +1,15 @@
+function addTask()
+{
+	try
+	{
+		// TODO Scrivere qua il codice
+	}
+	catch(e)
+	{
+		console.error("Qalcosa non ha funzionato:\n"+e.toString());
+		
+	}
+	
+	// Ritorna sempre false per il PHP
+	return false;
+}
